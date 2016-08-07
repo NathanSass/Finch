9297e3804d44cb96f7952b2639eee9fa7519690d
Nathan Sass <nsass711@gmail.com>
2016-08-07 12:39:37 -0700

    converted the compose view into a dialog

--- app/src/main/java/com/codepath/apps/finch/activities/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/finch/activities/TimelineActivity.java
@@ -3,13 +3,12 @@ package com.codepath.apps.finch.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -17,6 +16,7 @@ import com.codepath.apps.finch.R;
 import com.codepath.apps.finch.TwitterApplication;
 import com.codepath.apps.finch.TwitterClient;
 import com.codepath.apps.finch.adapters.TweetsAdapter;
+import com.codepath.apps.finch.fragments.ComposeTweetFragment;
 import com.codepath.apps.finch.models.Tweet;
 import com.codepath.apps.finch.util.EndlessRecyclerViewScrollListener;
 import com.codepath.apps.finch.util.ItemClickSupport;
@@ -31,9 +31,10 @@ import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cz.msebera.android.httpclient.Header;
 
-public class TimelineActivity extends AppCompatActivity {
+public class TimelineActivity extends AppCompatActivity implements ComposeTweetFragment.Communicator{
 
     private final int REQUEST_CODE = 99;
 
@@ -43,6 +44,7 @@ public class TimelineActivity extends AppCompatActivity {
     private Context context;
 
     @BindView(R.id.swipeContainer) SwipeRefreshLayout swipeRefreshLayout;
+//    @BindView(R.id.fab) FloatingActionButton floatingActionButton;
 
     LinearLayoutManager linearLayoutManager;
 
@@ -73,6 +75,19 @@ public class TimelineActivity extends AppCompatActivity {
         populateTimelineFromAPI();
 
         setTweetItemClickListener();
+    }
+
+    @OnClick(R.id.fab)
+    public void onFabButtonClick() {
+
+        showComposeDialog();
+
+    }
+
+    private void showComposeDialog() {
+        FragmentManager fm = getSupportFragmentManager();
+        ComposeTweetFragment composeTweetFragment = ComposeTweetFragment.newInstance("Compose New Tweet", null);
+        composeTweetFragment.show(fm, "activity_compose");
     }
 
     public void setTweetItemClickListener() {
@@ -194,47 +209,58 @@ public class TimelineActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_timeline, menu);
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_compose) {
-            Intent i = new Intent(TimelineActivity.this, ComposeActivity.class);
-            startActivityForResult(i, REQUEST_CODE);
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
-            int code = data.getExtras().getInt("code", 0);
-
-            Tweet tweet = Parcels.unwrap(data.getParcelableExtra("tweet"));
-            addTweetToTimelineStart(tweet);
-
-            rvTweets.smoothScrollToPosition(0);
-
-            Toast.makeText(this, "Tweet: " + tweet.getBody(), Toast.LENGTH_SHORT).show();
-        }
-    }
 
     public void addTweetToTimelineStart(Tweet tweet) {
         tweets.add(0, tweet);
         adapter.notifyItemInserted(0);
     }
+
+    @Override
+    public void onTweetPost(Tweet tweet) {
+        addTweetToTimelineStart(tweet);
+
+        rvTweets.smoothScrollToPosition(0);
+
+        Toast.makeText(this, "Tweet: " + tweet.getBody(), Toast.LENGTH_SHORT).show();
+    }
+
+    //    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_timeline, menu);
+//
+//        return true;
+//    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_compose) {
+//            Intent i = new Intent(TimelineActivity.this, ComposeActivity.class);
+//            startActivityForResult(i, REQUEST_CODE);
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+
+    // TODO DEPRECATE
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+//            int code = data.getExtras().getInt("code", 0);
+//
+//            Tweet tweet = Parcels.unwrap(data.getParcelableExtra("tweet"));
+//            addTweetToTimelineStart(tweet);
+//
+//            rvTweets.smoothScrollToPosition(0);
+//
+//            Toast.makeText(this, "Tweet: " + tweet.getBody(), Toast.LENGTH_SHORT).show();
+//        }
+//    }
 }
---
+++ app/src/main/java/com/codepath/apps/finch/fragments/ComposeTweetFragment.java
@@ -0,0 +1,131 @@
+package com.codepath.apps.finch.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.codepath.apps.finch.R;
+import com.codepath.apps.finch.TwitterApplication;
+import com.codepath.apps.finch.TwitterClient;
+import com.codepath.apps.finch.models.Tweet;
+import com.codepath.apps.finch.util.Util;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONObject;
+import org.parceler.Parcels;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnTextChanged;
+import butterknife.Unbinder;
+import cz.msebera.android.httpclient.Header;
+
+/**
+ * Created by nathansass on 8/7/16.
+ */
+public class ComposeTweetFragment extends DialogFragment {
+
+    private Unbinder unbinder;
+
+
+    private TwitterClient client;
+    private final int TWEET_LENGTH = 140;
+
+    Tweet replyToTweet;
+
+    @BindView(R.id.etTweetBody)
+    EditText etTweetBody;
+    @BindView(R.id.pbLoading)
+    ProgressBar progressBar;
+
+    @BindView(R.id.tvCharCount)
+    TextView tvCharCount;
+
+    public ComposeTweetFragment() {}
+
+    public static ComposeTweetFragment newInstance(String title, Tweet replyToTweet) {
+        ComposeTweetFragment frag = new ComposeTweetFragment();
+        Bundle args = new Bundle();
+
+        args.putString("title", title);
+        args.putParcelable("replyToTweet", Parcels.wrap(replyToTweet));
+
+        frag.setArguments(args);
+        return frag;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_compose, container);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+
+
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        if ((replyToTweet = savedInstanceState.getParcelable("replyToTweet")) != null)
+            replyToTweet = savedInstanceState.getParcelable("replyToTweet");
+
+        client = TwitterApplication.getRestClient();
+
+    }
+
+    @OnTextChanged(R.id.etTweetBody)
+    public void textChanged (CharSequence text) {
+        int remaining = TWEET_LENGTH - text.length();
+
+        tvCharCount.setText("" + remaining);
+    }
+
+    @OnClick(R.id.submit)
+    public void submitTweet() {
+
+        String tweetBody = etTweetBody.getText().toString();
+
+        progressBar.setVisibility( ProgressBar.VISIBLE );
+
+        client.postNewTweet(replyToTweet, tweetBody, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                progressBar.setVisibility( ProgressBar.INVISIBLE );
+
+                Tweet tweet = Tweet.fromJSON(response);
+
+                Communicator communicator = (Communicator) getActivity();
+                communicator.onTweetPost(tweet);
+
+                //TODO: add a second communicator for handleing reply to tweets
+
+                dismiss();
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Util.handleJsonFailure(errorResponse);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    public interface Communicator {
+        void onTweetPost(Tweet tweet);
+    }
+}
